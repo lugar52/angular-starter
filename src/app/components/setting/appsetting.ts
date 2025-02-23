f64183616367475837c4a03fd0c6bf1db76b14e3
Luis Garrido J. <garrlis@gmail.com>
2025-02-23 16:43:07 -0300

    se corrije lista de errores Rodrigo 2

--- src/app/components/setting/appsetting.ts
+++ src/app/components/setting/appsetting.ts
@@ -1,4 +1,4 @@
 export const appsettings = {
-    // apiUrl: 'https://myfastapi-production.up.railway.app/api'
-     apiUrl: 'http://127.0.0.1:8000/api'
+     apiUrl: 'https://myfastapi-production.up.railway.app/api'
+    // apiUrl: 'http://127.0.0.1:8000/api'
 }
